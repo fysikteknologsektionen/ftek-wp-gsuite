a03d666a59ac26fa9f9f534ff357368bc9f74fba
Johan Winther <johan.winther@outlook.com>
2018-08-20 11:52:23 +0200

    Added files

---
+++ ftek-gsuite.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Ftek G Suite
+Description: Provides functionality for fetching G Suite user and group data 
+Author: Johan Winther (johwin)
+Text Domain: ftek_gsuite
+Domain Path: /languages
+GitHub Plugin URI: Fysikteknologsektionen/ftek-gsuite
+*/
+
