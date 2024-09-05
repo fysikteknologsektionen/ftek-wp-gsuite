378904bb2acffcce3217852c750e744e221c0c6d
Wilmer Z <wilzet02@gmail.com>
2024-09-05 13:22:47 +0200

    Update cache on startup and increase version number

--- ftek-gsuite.php
+++ ftek-gsuite.php
@@ -3,7 +3,7 @@
 Plugin Name: Ftek G Suite
 Description: Provides functionality for fetching G Suite user and group data 
 Author: Johan Winther (johwin)
-Version: 1.5.1
+Version: 1.6.0
 Text Domain: ftek_gsuite
 Domain Path: /languages
 GitHub Plugin URI: fysikteknologsektionen/ftek-gsuite
